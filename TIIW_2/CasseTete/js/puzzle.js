843e69b5f0db0ffdcf616034a787e87660a58b38
mioko75 <mioko75@hotmail.com>
2019-09-08 19:24:47 -0400

    cassette et flash

--- TIIW_2/CasseTete/js/puzzle.js
+++ TIIW_2/CasseTete/js/puzzle.js
@@ -2,13 +2,15 @@
  * TP-3 Puzzle created by Sophie on 2019-08-31.
  */
 
-var premierClic = 1;
+//image de victoire en attente d'une victoire
+$('#bravo').hide();
 
-
+//Souris pointeur sur image
 $('img').hover(function () {
     $('img').css("cursor","pointer");
-})
+});
 
+var premierClic = 1;
 var id1;
 var src1;
 var id2;
@@ -17,24 +19,45 @@ var src2;
 $( ".img-fluid" ).click(function() {
 
     if (premierClic === 1){
-        src1 = this.src;
-        id1 = this.id;
-        $(this).css({display: "none"});
-        premierClic = 2;
+        src1 = this.src; // enregistre le src de l'image du premier clic
+        id1 = this.id; // enregistre le id de l'image du premier clic
+        $(this).css({display: "none"}); // cache l'image du premier clic
+        premierClic = 2; // On passe au 2e clic
     }
     else {
-        src2 = this.src;
-        id2 = this.id;
-        $('#' + id1).attr("src", src2 );
-        $('#' + id1).show();
-        $(this).css({display: "none"});
+        src2 = this.src;  // enregistre le src de l'image du deuxieme clic
+        id2 = this.id;  // enregistre le id de l'image du deuxieme clic
+        $('#' + id2).attr("src", src1 );  // transfert le le src de la deuxieme image sur la 1ere
+        $('#' + id1).attr("src", src2 ); // transfert le le src de la premier image sur la 2e
+        $('#' + id1).show(); // Affiche l'image
+        VerifierVictoire ();
         premierClic = 1;
     }
 });
 
+// victoire
+
+function VerifierVictoire(){
+    var victoire = true;
+    var images = ["image1","image2"];
+    for (i = 1; i < 10; i++) {
+
+        var check = $('#image'+i).attr('src').split('/').pop();
+        if(check != "image"+i+".jpg"){
+            victoire = false;
+        }
+    }
+    if(victoire){
+        $('#puzzle').hide();
+        $('#bravo').show();
+    }
+}
+
 //bouton recommencer
 
 $( "#recommencer" ).click(function() {
+    $('#puzzle').show();
+    $('#bravo').hide();
     $("#image1").attr("src", "images/image7.jpg");
     $("#image2").attr("src", "images/image5.jpg");
     $("#image3").attr("src", "images/image3.jpg");
@@ -43,7 +66,10 @@ $( "#recommencer" ).click(function() {
     $("#image6").attr("src", "images/image9.jpg");
     $("#image7").attr("src", "images/image8.jpg");
     $("#image8").attr("src", "images/image1.jpg");
-    $("#image9").attr("src", "images/image6.jpg");
+    $("#image9").attr("src", "images/image6.jpg");{
+        clearInterval(myVar);
+    }
+
 });
 
 // bouton réponse
@@ -57,34 +83,14 @@ $( "#reponse" ).click(function() {
     $("#image6").attr("src", "images/image6.jpg");
     $("#image7").attr("src", "images/image7.jpg");
     $("#image8").attr("src", "images/image8.jpg");
-    $("#image9").attr("src", "images/image9.jpg");
+    $("#image9").attr("src", "images/image9.jpg");{
+        myVar = setInterval(plusvictoire, 1000);
+    }
+    function plusvictoire() {
+        $('#puzzle').hide();
+        $('#bravo').show();
+    }
+
 });
 
-
-
-
-// victoire
-/**
-
-if ($('#image1').attr('src') === ("images/image1.jpg")) ; {
-    if ($('#image2').attr('src') === ("images/image2.jpg")) ; {
-        if ($('#image3').attr('src') === ("images/image3.jpg")) ; {
-            if ($('#image4').attr('src') === ("images/image4.jpg")) ; {
-                if ($('#image5').attr('src') === ("images/image5.jpg")) ; {
-                    if ($('#image6').attr('src') === ("images/image6.jpg")) ; {
-                        if ($('#image7').attr('src') === ("images/image7.jpg")) ; {
-                            if ($('#image8').attr('src') === ("images/image8.jpg")) ; {
-                                if ($('#image9').attr('src') === ("images/image9.jpg")) ;
-
-                                $( "#puzzle" ).hide();
-                                $( "body" ).append( "<img src=\"images/felicitations.jpg\" alt=\"felicitation\" title=\"félicitation\">" );
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-*/
 
